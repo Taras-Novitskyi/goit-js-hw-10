7f2a0040204d900b99d6468047ced7a7fd050bdd
Taras-Novitskyi <novitskyitaras@gmail.com>
2022-10-09 19:45:14 +0300

    complete hw

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import { fetchCountries } from './js/fetchCountries';
+import Notiflix from 'notiflix';
+import API from './js/fetchCountries';
+import countryCardTpl from './templates/country-card.hbs';
+import countryInfoTpl from './templates/country-info.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -10,9 +13,56 @@ const refs = {
 	countryInfo: document.querySelector('.country-info'),
 }
 
-refs.inputSearch.addEventListener('input', debounce(onSearch, 300));
+refs.inputSearch.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(e) {
-	fetchCountries(e.target.value);
+	e.preventDefault();
+	const searchResult = e.target.value.trim();
 
+	if (!searchResult) {
+		return createMarkup('', '');
+	}
+
+    API.fetchCountries(searchResult)
+      .then(renderCountryList)
+      .catch(onFetchError)
+      .finally(() => {});
+}
+
+function renderCountryList(countries) {
+	let markupCountryList = '';
+	let markupCountryInfo = '';
+
+    if (countries.length > 10) {
+    	Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+		);
+		createMarkup(markupCountryList, markupCountryInfo);
+    	return;
+    }
+	
+	for (let index = 0; index < countries.length; index += 1) {
+		const country = countries[index];
+		markupCountryList += countryCardTpl(country);
+
+		if (countries.length === 1) {
+			refs.countryList.classList.add('large');
+			markupCountryInfo = countryInfoTpl(country);
+		} else {
+			refs.countryList.classList.remove('large');
+		}
+		
+		createMarkup(markupCountryList, markupCountryInfo);
+    }
+}
+
+function createMarkup(markupList, markupInfo) {
+  refs.countryInfo.innerHTML = markupInfo;
+  refs.countryList.innerHTML = markupList;
+}
+
+function onFetchError(err) {
+	console.log(err);
+	Notiflix.Notify.failure('Oops, there is no country with that name');
+	createMarkup('', '');
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,38 +1,12 @@
-import countryCardTpl from '../templates/country-card.hbs';
-import countryInfoTpl from '../templates/country-info.hbs';
+const BASE_URL =  'https://restcountries.com/v3.1';
 
-const refs = {
-  countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
+function fetchCountries(nameCountry) {
+  return fetch(`${BASE_URL}/name/${nameCountry}?fields=name,capital,flags,population,languages`).then(response => {
+	if (response.ok) {
+    	return response.json();
+ 	 }
+ 	 throw new Error(response.status);
+  });
 };
 
-export function fetchCountries(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,flags,population,languages`
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(country => {
-      let markup = '';
-
-      for (let index = 0; index < 4; index++) {
-        if (country.length === 0) {
-          alert('nema');
-          return;
-        }
-
-        if (country[index]) {
-          const markupCountry = countryCardTpl(country[index]);
-          markup += markupCountry;
-          
-          if (country.length === 1) {
-            markup += countryInfoTpl(country[index]);
-			}
-
-		refs.countryList.innerHTML = markup;
-        }
-      }
-    })
-    .catch(err => console.log(err));
-}
+export default { fetchCountries };
